ce1adefbd8dca6cfa1fe5013dc8f36029d779533
Raoul Schwagmeier <raoul.schwagmeier@gorillasapp.com>
2022-06-23 08:59:22 +0200

    Initial commit

---
+++ Colors.swift
@@ -0,0 +1,146 @@
+import UIKit
+
+enum Colors {
+    static let gray0: UIColor = .init(named: "Gray 0")!
+    static let gray1: UIColor = .init(named: "Gray 1")!
+    static let gray2: UIColor = .init(named: "Gray 2")!
+    static let gray3: UIColor = .init(named: "Gray 3")!
+    static let gray4: UIColor = .init(named: "Gray 4")!
+    static let gray5: UIColor = .init(named: "Gray 5")!
+    static let gray6: UIColor = .init(named: "Gray 6")!
+    static let gray7: UIColor = .init(named: "Gray 7")!
+    static let gray8: UIColor = .init(named: "Gray 8")!
+    static let gray9: UIColor = .init(named: "Gray 9")!
+
+    static let red0: UIColor = .init(named: "Red 0")!
+    static let red1: UIColor = .init(named: "Red 1")!
+    static let red2: UIColor = .init(named: "Red 2")!
+    static let red3: UIColor = .init(named: "Red 3")!
+    static let red4: UIColor = .init(named: "Red 4")!
+    static let red5: UIColor = .init(named: "Red 5")!
+    static let red6: UIColor = .init(named: "Red 6")!
+    static let red7: UIColor = .init(named: "Red 7")!
+    static let red8: UIColor = .init(named: "Red 8")!
+    static let red9: UIColor = .init(named: "Red 9")!
+
+    static let pink0: UIColor = .init(named: "Pink 0")!
+    static let pink1: UIColor = .init(named: "Pink 1")!
+    static let pink2: UIColor = .init(named: "Pink 2")!
+    static let pink3: UIColor = .init(named: "Pink 3")!
+    static let pink4: UIColor = .init(named: "Pink 4")!
+    static let pink5: UIColor = .init(named: "Pink 5")!
+    static let pink6: UIColor = .init(named: "Pink 6")!
+    static let pink7: UIColor = .init(named: "Pink 7")!
+    static let pink8: UIColor = .init(named: "Pink 8")!
+    static let pink9: UIColor = .init(named: "Pink 9")!
+
+    static let grape0: UIColor = .init(named: "Grape 0")!
+    static let grape1: UIColor = .init(named: "Grape 1")!
+    static let grape2: UIColor = .init(named: "Grape 2")!
+    static let grape3: UIColor = .init(named: "Grape 3")!
+    static let grape4: UIColor = .init(named: "Grape 4")!
+    static let grape5: UIColor = .init(named: "Grape 5")!
+    static let grape6: UIColor = .init(named: "Grape 6")!
+    static let grape7: UIColor = .init(named: "Grape 7")!
+    static let grape8: UIColor = .init(named: "Grape 8")!
+    static let grape9: UIColor = .init(named: "Grape 9")!
+
+    static let violet0: UIColor = .init(named: "Violet 0")!
+    static let violet1: UIColor = .init(named: "Violet 1")!
+    static let violet2: UIColor = .init(named: "Violet 2")!
+    static let violet3: UIColor = .init(named: "Violet 3")!
+    static let violet4: UIColor = .init(named: "Violet 4")!
+    static let violet5: UIColor = .init(named: "Violet 5")!
+    static let violet6: UIColor = .init(named: "Violet 6")!
+    static let violet7: UIColor = .init(named: "Violet 7")!
+    static let violet8: UIColor = .init(named: "Violet 8")!
+    static let violet9: UIColor = .init(named: "Violet 9")!
+
+    static let indigo0: UIColor = .init(named: "Indigo 0")!
+    static let indigo1: UIColor = .init(named: "Indigo 1")!
+    static let indigo2: UIColor = .init(named: "Indigo 2")!
+    static let indigo3: UIColor = .init(named: "Indigo 3")!
+    static let indigo4: UIColor = .init(named: "Indigo 4")!
+    static let indigo5: UIColor = .init(named: "Indigo 5")!
+    static let indigo6: UIColor = .init(named: "Indigo 6")!
+    static let indigo7: UIColor = .init(named: "Indigo 7")!
+    static let indigo8: UIColor = .init(named: "Indigo 8")!
+    static let indigo9: UIColor = .init(named: "Indigo 9")!
+
+    static let blue0: UIColor = .init(named: "Blue 0")!
+    static let blue1: UIColor = .init(named: "Blue 1")!
+    static let blue2: UIColor = .init(named: "Blue 2")!
+    static let blue3: UIColor = .init(named: "Blue 3")!
+    static let blue4: UIColor = .init(named: "Blue 4")!
+    static let blue5: UIColor = .init(named: "Blue 5")!
+    static let blue6: UIColor = .init(named: "Blue 6")!
+    static let blue7: UIColor = .init(named: "Blue 7")!
+    static let blue8: UIColor = .init(named: "Blue 8")!
+    static let blue9: UIColor = .init(named: "Blue 9")!
+
+    static let cyan0: UIColor = .init(named: "Cyan 0")!
+    static let cyan1: UIColor = .init(named: "Cyan 1")!
+    static let cyan2: UIColor = .init(named: "Cyan 2")!
+    static let cyan3: UIColor = .init(named: "Cyan 3")!
+    static let cyan4: UIColor = .init(named: "Cyan 4")!
+    static let cyan5: UIColor = .init(named: "Cyan 5")!
+    static let cyan6: UIColor = .init(named: "Cyan 6")!
+    static let cyan7: UIColor = .init(named: "Cyan 7")!
+    static let cyan8: UIColor = .init(named: "Cyan 8")!
+    static let cyan9: UIColor = .init(named: "Cyan 9")!
+
+    static let teal0: UIColor = .init(named: "Teal 0")!
+    static let teal1: UIColor = .init(named: "Teal 1")!
+    static let teal2: UIColor = .init(named: "Teal 2")!
+    static let teal3: UIColor = .init(named: "Teal 3")!
+    static let teal4: UIColor = .init(named: "Teal 4")!
+    static let teal5: UIColor = .init(named: "Teal 5")!
+    static let teal6: UIColor = .init(named: "Teal 6")!
+    static let teal7: UIColor = .init(named: "Teal 7")!
+    static let teal8: UIColor = .init(named: "Teal 8")!
+    static let teal9: UIColor = .init(named: "Teal 9")!
+
+    static let green0: UIColor = .init(named: "Green 0")!
+    static let green1: UIColor = .init(named: "Green 1")!
+    static let green2: UIColor = .init(named: "Green 2")!
+    static let green3: UIColor = .init(named: "Green 3")!
+    static let green4: UIColor = .init(named: "Green 4")!
+    static let green5: UIColor = .init(named: "Green 5")!
+    static let green6: UIColor = .init(named: "Green 6")!
+    static let green7: UIColor = .init(named: "Green 7")!
+    static let green8: UIColor = .init(named: "Green 8")!
+    static let green9: UIColor = .init(named: "Green 9")!
+
+    static let lime0: UIColor = .init(named: "Lime 0")!
+    static let lime1: UIColor = .init(named: "Lime 1")!
+    static let lime2: UIColor = .init(named: "Lime 2")!
+    static let lime3: UIColor = .init(named: "Lime 3")!
+    static let lime4: UIColor = .init(named: "Lime 4")!
+    static let lime5: UIColor = .init(named: "Lime 5")!
+    static let lime6: UIColor = .init(named: "Lime 6")!
+    static let lime7: UIColor = .init(named: "Lime 7")!
+    static let lime8: UIColor = .init(named: "Lime 8")!
+    static let lime9: UIColor = .init(named: "Lime 9")!
+
+    static let yellow0: UIColor = .init(named: "Yellow 0")!
+    static let yellow1: UIColor = .init(named: "Yellow 1")!
+    static let yellow2: UIColor = .init(named: "Yellow 2")!
+    static let yellow3: UIColor = .init(named: "Yellow 3")!
+    static let yellow4: UIColor = .init(named: "Yellow 4")!
+    static let yellow5: UIColor = .init(named: "Yellow 5")!
+    static let yellow6: UIColor = .init(named: "Yellow 6")!
+    static let yellow7: UIColor = .init(named: "Yellow 7")!
+    static let yellow8: UIColor = .init(named: "Yellow 8")!
+    static let yellow9: UIColor = .init(named: "Yellow 9")!
+
+    static let orange0: UIColor = .init(named: "Orange 0")!
+    static let orange1: UIColor = .init(named: "Orange 1")!
+    static let orange2: UIColor = .init(named: "Orange 2")!
+    static let orange3: UIColor = .init(named: "Orange 3")!
+    static let orange4: UIColor = .init(named: "Orange 4")!
+    static let orange5: UIColor = .init(named: "Orange 5")!
+    static let orange6: UIColor = .init(named: "Orange 6")!
+    static let orange7: UIColor = .init(named: "Orange 7")!
+    static let orange8: UIColor = .init(named: "Orange 8")!
+    static let orange9: UIColor = .init(named: "Orange 9")!
+}
